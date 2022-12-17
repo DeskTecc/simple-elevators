8f3e974d38d60b280cc8d854b6a0c69761a4a5d7
Mateus <43929423+DeskTecc@users.noreply.github.com>
2022-12-17 16:33:15 -0300

    WIP 12%

--- src/main/java/eu/koboo/simple/elevator/listener/PlayerCommandListener.java
+++ src/main/java/eu/koboo/simple/elevator/listener/PlayerCommandListener.java
@@ -1,47 +1,57 @@
 package eu.koboo.simple.elevator.listener;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import static org.bukkit.Bukkit.getLogger;
 
 public class PlayerCommandListener implements CommandExecutor {
 
-    // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
 
             Player player = (Player) sender;
-
-            ItemStack item = new ItemStack(Material.STICK, 1);
-            ItemMeta meta = item.getItemMeta();
-            if (player.getInventory().contains(Material.STICK, 1)) {
-                player.getInventory().remove(Material.STICK);
-
-            } else {
-                assert meta != null;
-                meta.setDisplayName("§l§bElevator Maker!");
-                item.setItemMeta(meta);
-                player.getInventory().addItem(item);
-
+            for(ItemStack item : player.getInventory().getContents()) {
+                if (!(item == null)) {
+                    if (item.getItemMeta().hasDisplayName()) {
+                        if (item.getItemMeta().getDisplayName().equals(ChatColor.AQUA+"Elevator Maker!")) {
+                            item.setAmount(0);
+                            return true;
+                        }
+                    }
+                }
             }
+            ItemStack item = new ItemStack(Material.STICK);
+            ItemMeta meta = item.getItemMeta();
+            ArrayList<String> lore = new ArrayList<String>();
+            assert meta != null;
+            meta.setDisplayName(ChatColor.AQUA+"Elevator Maker!");
+            meta.addEnchant(Enchantment.LUCK,1,true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            lore.add(0,ChatColor.YELLOW+"Left Click: "+ChatColor.WHITE+"Set one elevator");
+            lore.add(1,ChatColor.GOLD+"Right Click: "+ChatColor.WHITE+"Set second elevator");
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+            player.getInventory().addItem(item);
         }
-
-
         else{
-            getLogger().info("You need to be a player to do this!");
+            getLogger().info("You must to be a player to do this!");
         }
 
-        // If the player (or console) uses our command correct, we can return true
         return true;
     }
 }
